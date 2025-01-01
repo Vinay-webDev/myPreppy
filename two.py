99c2665b896aa75fe6ec12ca92b5283868293c05
Vinay-webDev <justchatting3000@gmail.com>
2025-01-01 21:44:28 +0530

    Update two.py

--- two.py
+++ two.py
@@ -15,4 +15,18 @@ sol = Solution()
 print(sol.calculateFrequency(nums1))
 #{2: 2, 1: 3, 3: 2, 4: 2}    
 print(sol.calculateFrequency(nums2))
-#{5: 3, 6: 1, 7: 1, 8: 1}   
+#{5: 3, 6: 1, 7: 1, 8: 1}
+""""""""""""""""""""""""""""""""""""""""""""""""
+class Solution:
+    def calculateFrequency(self, nums):
+        frequency = {}
+        for num in nums:
+            if num in frequency:
+                frequency[num] += 1
+            else:
+                frequency[num] = 1
+        return [value for value in frequency]
+sol = Solution()
+print(sol.calculateFrequency(nums1))  #[2, 1, 3, 4]
+print(sol.calculateFrequency(nums2))  #[5, 6, 7, 8]
+
